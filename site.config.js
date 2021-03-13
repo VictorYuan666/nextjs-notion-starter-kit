293133773ac2211e60a981b49e41136f5a7cb0fe
yuanxiaowei <yuanxiaowei@chuangjiangx.com>
2021-03-13 21:27:06 +0800

    change config

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '1ad641b260ae4d7fba6d8b4392cff640',
+  rootNotionPageId: '4bb3fb49134246918849dd7e4345eff6',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
